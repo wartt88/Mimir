a4814f46d26da521290d6e55e258d59d0429b8ca
Delioos <htz_j@protonmail.com>
2024-01-08 16:51:56 +0100

    rebuild(mono repo): w/ tailwind this time lfg

--- app/apps/web/src/app/layout.tsx
+++ app/apps/web/src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import "@repo/ui/styles.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import React from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
