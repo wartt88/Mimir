c2f4b41b3b1aa3b55d56ff1d60fb937cde74cb90
wartt88 <theo036188@gmail.com>
2024-01-08 22:29:12 +0100

    update(pages): add decklist component

--- app/apps/web/src/app/page.tsx
+++ app/apps/web/src/app/page.tsx
@@ -31,7 +31,7 @@ const deck = {
     },
     {
         id_card: 3,
-        question: "Quelle âge à le monde ?",
+        question: "Quel âge à le monde ?",
         reponse: "4,54 milliards d'années",
         palier: 1,
         derniereRevision: 1704708559
@@ -46,33 +46,43 @@ export default function Page(): JSX.Element {
   const never = cards.filter((e)=>e.palier===0).length;
   const other = cards.length - (never+learned);
 
-  const elements = [];
+  const elements = [<Link key={0} href='/'><NewDeck/></Link>];
   //recevoir les deckPreview
-  for (let i = 0; i < 3; i++) {
+  for (let i = 1; i < 4; i++) {
     elements.push(
-      <Link href={{
+      <Link key={i} href={{
         pathname: '/deck',
         query: { deck: deck.id, card: i}
       }}> 
-      <DeckPreview key={i} learned={learned} never={never} other={other}/>
+      <DeckPreview learned={learned} never={never} other={other}/>
     </Link>);
   }
 
   return (
-    <div>
+    <div className="flex bg-white text-black size-full items-center space-x-5 overflow-scroll">
       {elements}
     </div>
   );
 }
 
 function DeckPreview(props:{learned:number,never:number,other:number}): JSX.Element {
-  return (<>
-    <h3>{deck.titre}</h3>
+  return (<div className="bg-gray-100 p-6 flex flex-col gap-2 border-2 border-gray-600 rounded-lg size-48">
+    <h3 className="text-2xl text-center">{deck.titre}</h3>
         <div>
-            <p>{props.learned} Learned</p>
-            <p>{props.never} Never seen</p>
-            <p>{props.other} Not Learned</p>
+            <p className="text-blue-600">{props.learned} Learned</p>
+            <p className="text-gray-400">{props.never} Never seen</p>
+            <p className="text-red-500">{props.other} Not Learned</p>
         </div>
-    </>
+    </div>
+  )
+}
+
+function NewDeck(): JSX.Element {
+  return (<div className="size-48 bg-gray-100 p-6 flex flex-col gap-2 border-2 border-gray-600 rounded-lg items-center">
+    <h3 className="text-xl text-center">Ajouter un nouveau deck</h3>
+    <svg width="63" height="67" viewBox="0 0 63 67" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M62.5 25.3125V41H0.125V25.3125H62.5ZM39.875 0.625V66.875H22.8125V0.625H39.875Z" fill="black"/>
+    </svg>
+    </div>
   )
 }
