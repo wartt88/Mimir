444070a1b2b4dac9b2acb7ed0f72198f7dbfb76e
Alexandre P <alexandre.perrot54@gmail.com>
2024-01-09 11:22:04 +0100

    feat(extractor): add upload/extract endpoint

--- app/apps/extractor/src/main.py
+++ app/apps/extractor/src/main.py
@@ -1,8 +1,63 @@
 import fitz
+from flask import Flask, flash, request, redirect, url_for, session
+from markupsafe import escape
+from werkzeug.utils import secure_filename
+import os
+import pathlib
 
-doc = fitz.open("rec.pdf")
-output = ""
-for page in doc:  # iterate the document pages
-    text = page.get_text()
-    output += text
-print(output)
+UPLOAD_FOLDER = '.'
+ALLOWED_EXTENSIONS = {'pdf'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+
+@app.route("/extract/")
+def extract():
+    name = request.args.get("name")
+
+    pdfbytes = pathlib.Path(name).read_bytes()
+    doc = fitz.open("pdf", pdfbytes)
+    output = ""
+    for page in doc:  # iterate the document pages
+        text = page.get_text()
+        output += text
+
+    os.remove(name)
+    return {
+        "text": output
+    }
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route("/upload/", methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('extract', name=filename))
+    return '''
+        <!doctype html>
+        <title>Upload new File</title>
+        <h1>Upload new File</h1>
+        <form method=post enctype=multipart/form-data>
+          <input type=file name=file>
+          <input type=submit value=Upload>
+        </form>
+        '''
