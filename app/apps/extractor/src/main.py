d9baae1ec0fbe1ab1e050a63fd204cb3fcbc2c16
Alexandre P <alexandre.perrot54@gmail.com>
2024-01-09 11:29:23 +0100

    feat(extractor): add error msg

--- app/apps/extractor/src/main.py
+++ app/apps/extractor/src/main.py
@@ -26,6 +26,7 @@ def extract():
 
     os.remove(name)
     return {
+        "ok": "true",
         "text": output
     }
 
@@ -35,29 +36,23 @@ def allowed_file(filename):
         filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-@app.route("/upload/", methods=['GET', 'POST'])
+@app.route("/error/")
+def error():
+    text = request.args.get("text")
+    return {
+        "ok": "false",
+        "error": text
+    }
+
+
+@app.post("/upload/")
 def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # If the user does not select a file, the browser submits an
-        # empty file without a filename.
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('extract', name=filename))
-    return '''
-        <!doctype html>
-        <title>Upload new File</title>
-        <h1>Upload new File</h1>
-        <form method=post enctype=multipart/form-data>
-          <input type=file name=file>
-          <input type=submit value=Upload>
-        </form>
-        '''
+    if 'file' not in request.files:
+        return redirect(url_for('error', text="No file found in the form"))
+    file = request.files['file']
+    if file.filename == '':
+        return redirect(url_for('error', text="No selected file"))
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return redirect(url_for('extract', name=filename))
