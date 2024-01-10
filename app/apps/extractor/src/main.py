c7eff32d59161b7f9870d23a6cac171de0ff9a97
Alexandre P <alexandre.perrot54@gmail.com>
2024-01-10 10:03:12 +0100

    feat(extractor): add get

--- app/apps/extractor/src/main.py
+++ app/apps/extractor/src/main.py
@@ -10,7 +10,7 @@ ALLOWED_EXTENSIONS = {'pdf'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+app.secret_key = b'beb7b92dd94df7c9387e55726d5e70c0502262e9aa54dd5a51694e74726a4652'
 
 
 @app.route("/extract/")
@@ -27,7 +27,7 @@ def extract():
     os.remove(name)
     return {
         "ok": "true",
-        "text": output
+        "text": output.replace("\n", "")
     }
 
 
@@ -45,14 +45,24 @@ def error():
     }
 
 
-@app.post("/upload/")
+@app.route("/upload/", methods=['GET', 'POST'])
 def upload_file():
-    if 'file' not in request.files:
-        return redirect(url_for('error', text="No file found in the form"))
-    file = request.files['file']
-    if file.filename == '':
-        return redirect(url_for('error', text="No selected file"))
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        return redirect(url_for('extract', name=filename))
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return redirect(url_for('error', text="No file found in the form"))
+        file = request.files['file']
+        if file.filename == '':
+            return redirect(url_for('error', text="No selected file"))
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('extract', name=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
