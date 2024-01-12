b86be4d0c805fa2598c80f4f12da6dc5493fdb43
Delioos <htz_j@protonmail.com>
2024-01-12 10:35:08 +0100

    🚧build🚧(db): create deck schema🏗️ + deck post rq

--- app/apps/web/.eslintrc.js
+++ app/apps/web/.eslintrc.js
@@ -2,6 +2,6 @@ module.exports = {
   extends: ["@repo/eslint-config/next.js"],
   rules: {
     "no-console": "off",
-    "turbo/no-undeclared-env-vars": "off",
+    "turbo/no-undeclared-env-vars": "off"
   },
 };
--- app/apps/web/src/app/utils/db.ts
+++ app/apps/web/src/app/utils/db.ts
@@ -1,33 +1,13 @@
 import mongoose from "mongoose";
 
-const DATABASE_URL = process.env.MONGO;
-
-if (!DATABASE_URL) {
-  throw new Error("Please define the MONGODB_URI environment variable inside .env.local");
-}
-
-let cached = global.mongoose;
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
-
-async function connectDB() {
-  if (cached.conn) {
-    return cached.conn;
-  }
-
-  if (!cached.promise) {
-    const opts = {
-      bufferCommands: false,
-    };
-
-    cached.promise = mongoose.connect(DATABASE_URL, opts).then((mongoose) => {
-      return mongoose;
-    });
-  }
-  cached.conn = await cached.promise;
-  return cached.conn;
-}
+const connectDB = async () => {
+    try {
+        mongoose.set('strictQuery', false);
+        await mongoose.connect(process.env.MONGO);
+        console.log("MongoDB connected");
+    } catch (error) {
+        console.log(error);
+    }
+};
 
 export default connectDB;
