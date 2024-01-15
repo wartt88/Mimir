07280028cb8dcac6b41af589147ec94f197b71b7
Delioos <htz_j@protonmail.com>
2024-01-15 09:34:53 +0100

    build(cgu): ajout des conditions d'utilisation ✏️🪧

---
+++ app/apps/web/src/app/cgu/page.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+
+export default function CGU(): JSX.Element {
+ return (
+   <div className="flex flex-col w-1/2 bg-gray-200 p-5 font-mono">
+     <h1 className="mb-4">Conditions Générales d&#39;Utilisation pour l&#39;Application Mimir</h1>
+     <p className="mb-4">
+       Ces conditions générales d&#39;utilisation (&quot;CGU&quot;) définissent
+       les termes et conditions d&#39;utilisation de l&#39;application Mimir
+       (&quot;Mimir&quot;). En utilisant l&#39;application Mimir, vous acceptez
+       ces CGU. Si vous n&#39;acceptez pas ces CGU, veuillez ne pas utiliser
+       l&#39;application Mimir.
+     </p>
+       <h1 className="mb-4">Description de l&#39;Application</h1>
+     <p className="mb-4">
+       L&#39;application Mimir est une plateforme de cartes flash qui permet
+       aux utilisateurs de créer, partager et étudier des cartes flash
+       personnalisées. Les données des utilisateurs sont stockées dans le cloud
+       pour faciliter l&#39;accès à distance et la synchronisation entre
+       différents appareils. 2. Stockage des Données
+     </p>
+     <p className="mb-4">
+       Nous nous réservons le droit de stocker vos données dans le cloud. Cela
+       comprend, mais n&#39;est pas limité à, les informations de profil, les
+       cartes flash que vous créez, les notes que vous prenez et les
+       statistiques d&#39;étude. Nous utilisons ces données pour améliorer
+       l&#39;expérience utilisateur et fournir des fonctionnalités
+       supplémentaires. 3. Utilisation des Données
+     </p>
+     <p className="mb-4">
+       Nous pouvons utiliser vos données pour des fins statistiques, telles que
+       comprendre comment les utilisateurs utilisent l&#39;application et
+       identifier les domaines où nous pouvons améliorer. Nous ne vendons ni ne
+       partageons vos données avec des tiers sans votre consentement explicite.
+       4. Droits des Développeurs
+     </p>
+     <p className="mb-4">
+       En tant que développeurs, vous avez la liberté de modifier, distribuer
+       et utiliser l&#39;application Mimir dans les limites de ces CGU. Vous
+       pouvez également contribuer à l&#39;amélioration de l&#39;application en
+       soumettant des suggestions, signalant des bugs ou en contribuant au code
+       source. 5. Marge de Manœuvre
+     </p>
+     <p className="mb-4">
+       En tant que développeur, vous avez la possibilité de personnaliser
+       l&#39;application selon vos besoins. Cependant, toute modification de
+       l&#39;interface utilisateur, du comportement de l&#39;application ou de
+       sa structure devrait être effectuée de manière à ne pas nuire à
+       l&#39;expérience utilisateur globale. De plus, toute modification doit
+       être clairement documentée et communiquée aux utilisateurs. 6.
+       Responsabilité
+     </p>
+     <p className="mb-4">
+       Vous êtes entièrement responsable de l&#39;utilisation de
+       l&#39;application Mimir. Nous ne sommes pas responsables de tout dommage
+       direct ou indirect résultant de votre utilisation de l&#39;application.
+       7. Modification des CGU
+     </p>
+     <p className="mb-4">
+       Nous nous réservons le droit de modifier ces CGU à tout moment. Nous
+       vous recommandons de consulter régulièrement cette page pour rester
+       informé des dernières modifications. 8. Contact
+     </p>
+     <p>
+       Si vous avez des questions ou des préoccupations concernant ces CGU,
+       veuillez contacter notre équipe de support à l&#39;adresse suivante :{" "}
+       <a href="mailto:&#x73;&#x75;&#x70;&#112;&#111;&#114;&#x74;&#x40;&#x6d;&#105;&#109;&#105;&#114;&#x2e;&#x63;&#111;&#109;">
+         &#x73;&#x75;&#x70;&#112;&#111;&#114;&#x74;&#x40;&#x6d;&#105;&#109;&#105;&#114;&#x2e;&#x63;&#111;&#109;
+       </a>
+     </p>
+   </div>
+ );
+}
