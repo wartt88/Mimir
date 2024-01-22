2cd09cb26ad88687272c09aa30024aad33dcf72d
Alexandre P <alexandre.perrot54@gmail.com>
2024-01-22 16:15:58 +0100

    feat(auth): add login/register/forgotten view

--- app/apps/web/src/app/accueil/page.tsx
+++ app/apps/web/src/app/accueil/page.tsx
@@ -3,39 +3,7 @@ import Image, {ImageProps} from "next/image";
 import computer from "../../../public/pc.jpg";
 import Link from "next/link";
 import React, {ChangeEvent, ChangeEventHandler, MouseEventHandler, useState} from "react";
-
-interface ButtonProps {
-    title: string;
-    url: string;
-    bg?: boolean;
-}
-
-const Button = ({title, url, bg = false}: ButtonProps) => {
-    return bg ? (
-            <a className="font-Lexend p-2 bg-sky-300 rounded-lg transition ease-in-out delay-150 hover:scale-110"
-               href={url}>{title}</a>
-        ) :
-        (
-            <a className="font-Lexend p-2 transition ease-in-out delay-150 hover:scale-110" href={url}>{title}</a>
-        )
-}
-
-const NavBar = () => {
-    return <nav className="bg-white flex flex-row px-10 py-3 shadow">
-        <div>
-            <p className="font-Lemon text-5xl" style={{color: "#023047"}}>Mimir</p>
-        </div>
-        <div className="flex grow justify-center items-center space-x-7 font-Lexend ">
-            <Button title={"Accueil"} url={"#"}/>
-            <Button title={"Explorer le marketplace"} url={"#marketplace"}/>
-            <Button title={"Nos solutions"} url={"#solutions"}/>
-        </div>
-        <div className="flex items-center space-x-7 font-Lexend">
-            <Button title={"Se connecter"} url={"/login"}/>
-            <Button title={"S'inscrire"} url={"/register"} bg={true}/>
-        </div>
-    </nav>;
-}
+import NavBar from "../../components/ui/nav-bar.tsx";
 
 const Preview = () => {
     return <div className="h-192 w-full relative">
