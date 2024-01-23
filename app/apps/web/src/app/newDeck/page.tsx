773306a78a37d5caf6bd6e229df81a8d5cfdbece
Alexandre P <alexandre.perrot54@gmail.com>
2024-01-23 14:44:50 +0100

    feat(deck): add deck creation

--- app/apps/web/src/app/newDeck/page.tsx
+++ app/apps/web/src/app/newDeck/page.tsx
@@ -1,142 +1,180 @@
 "use client";
-import Image from "next/image";
-import Link from "next/link";
-import { useRouter } from "next/navigation";
+
+import {MutableRefObject, useRef, useState} from "react";
+import Footer from "../../components/ui/footer.tsx";
 
 interface Card {
-  id_card: number;
-  question: string;
-  reponse: string;
-  palier: number;
-  derniereRevision: number;
+    id_card: number;
+    question: string;
+    reponse: string;
+    palier: number;
+    derniereRevision: number;
 }
 
 //fake data
-const cartes: Card[] = [
-  {
-    id_card: 1,
-    question: "Allons-nous réussir ?zekfbkzefbzebfjzbffljzelknzelkfzlfnlfnerfkzefbkegfuzefuzerbhzefzerub",
-    reponse: "Oui",
-    palier: 5,
-    derniereRevision: 1704708559,
-  },
-  {
-    id_card: 2,
-    question: "2+2 ?",
-    reponse: "4",
-    palier: 1,
-    derniereRevision: 1704708559,
-  },
-  {
-    id_card: 3,
-    question: "Quel âge à le monde ?",
-    reponse: "4,54 milliards d'années",
-    palier: 1,
-    derniereRevision: 1704708559,
-  },
+const initialCards: Card[] = [
+    {
+        id_card: 1,
+        question: "Allons-nous réussir ?zekfbkzefbzebfjzbffljzelknzelkfzlfnlfnerfkzefbkegfuzefuzerbhzefzerub",
+        reponse: "Oui",
+        palier: 5,
+        derniereRevision: 1704708559,
+    },
+    {
+        id_card: 2,
+        question: "2+2 ?",
+        reponse: "4",
+        palier: 1,
+        derniereRevision: 1704708559,
+    },
+    {
+        id_card: 3,
+        question: "Quel âge à le monde ?",
+        reponse: "4,54 milliards d'années",
+        palier: 1,
+        derniereRevision: 1704708559,
+    },
 ];
 
-export default function Page(): JSX.Element {
-  const router = useRouter();
-  const elements: JSX.Element[] = [];
+const CardEditor = (card: Card): JSX.Element => {
 
-  function HandleConfirm(): void {
-    //TODO validation du deck et ajout à sa collection
-    router.push("/newDeck/confirm");
-  }
+    const questionRef = useRef(null);
+    const reponseRef = useRef(null);
 
-  function HandleDeleteAll(): void {
-    //TODO
-  }
+    const autoGrow = (ref: MutableRefObject<any>) => {
+        if (ref.current) {
+            ref.current.style.height = "auto";
+            ref.current.style.height = ref.current.scrollHeight + "px";
+        }
+    }
 
-  function HandleDelete():void {
-    //TODO
-  }
-
-  cartes.forEach((c, i) => {
-    elements.push(CardPreview(c, i, HandleDelete));
-  });
-
-  if (elements.length === 0) {
-    elements.push(EmptyCards());
-  }
-
-  return (
-    <div
-      className="
-     bg-gray-100 size-2/3 border-gray
-        flex text-black flex-col items-center justify-center gap-[10vh]"
-    >
-      <div className="flex flex-col w-2/3 items-center justify-around overflow-scroll scrollbar-hide gap-[2vh]">
-        <div className="grid grid-cols-4 grid-rows-2 items-center justify-center text-2xl font-semibold gap-y-[2vh]">
-          <div className="flex flex-col col-span-2 ">
-            <label htmlFor="deckName">
-              Nom du deck
-            </label>
-            <input className="px-2 py-1" id="deckName" type="text" />
-          </div>
-          <label className="text-center" htmlFor="educatifCheck">
-            Educatif
-          </label>
-          <input className="size-[3vh]" id="educatifCheck" type="checkbox" />
-          <div className="flex flex-col col-span-2">
-            <label  htmlFor="tagsIn">
-              Tags
-            </label>
-            <input className="px-2 py-1" id="tagsIn" type="text" />
-          </div>
-          <label className="text-center" htmlFor="privateCheck">
-            Private
-          </label>
-          <input className="size-[3vh]" id="privateCheck" type="checkbox" />
+    return <div className="bg-white rounded-md" key={card.id_card}>
+        <div className="flex justify-between px-5 pt-5">
+            <h1 className="font-Lexend text-xl font-bold">{card.id_card}</h1>
+            <div className="flex space-x-1">
+                <img src="/move.svg" alt=""/>
+                <img src="/delete.svg" alt=""/>
+            </div>
         </div>
-        <hr className="w-full border-black" />
-        <div className="w-3/4 flex flex-col gap-y-[1vh]">
-          <div className="flex">
-            <h3 className="text-2xl font-semibold">Liste des cartes : </h3>
-            <div className="flex-1" />
-            <Link href="/newDeck/card">
-              <Image alt="" className="size-[3vh]" height={20} src="add.svg" width={20} />
-            </Link>
-            <button onClick={HandleDeleteAll} type="button">
-              <Image alt="" className="size-[3vh]" height={20} src="trash.svg" width={20}/>
+        <hr className="my-2"/>
+
+        <div className="flex space-x-3">
+            <div className="flex flex-col justify-between px-5 pb-5 grow">
+                    <textarea className="resize-none h-auto" onChange={() => {
+                        autoGrow(questionRef);
+                    }} ref={questionRef}>{card.question}</textarea>
+                <hr className="border-2 border-black my-2"/>
+                <p className="font-Lexend">QUESTION</p>
+            </div>
+            <div className="flex flex-col justify-between px-5 pb-5 grow">
+                    <textarea className="resize-none h-auto" onChange={() => {
+                        autoGrow(reponseRef);
+                    }} ref={reponseRef}>{card.reponse}</textarea>
+                <hr className="border-2 border-black my-2"/>
+                <p className="font-Lexend">RÉPONSE</p>
+            </div>
+        </div>
+    </div>
+
+}
+
+function Page(): JSX.Element {
+
+    const [cards, setCards] = useState<Card[]>(initialCards);
+
+    const cardsJSX = cards.map((c) => {
+        return CardEditor(c);
+    });
+
+    const addCard = () => {
+        setCards([...cards, {
+            id_card: cards.length,
+            question: "",
+            reponse: "",
+            palier: 0,
+            derniereRevision: 0,
+        }]);
+    }
+
+
+return <div className="size-full">
+    <div className="p-[5%]">
+
+        <div className="flex justify-between">
+            <h1 className="font-Lexend text-3xl font-medium">Créer un nouveau deck</h1>
+            <div className="space-x-3">
+                <button className="bg-blue-500 text-white font-Lexend text-lg px-4 py-2 rounded-sm shadow">Créer
+                </button>
+                <button className="bg-gray-400 text-white font-Lexend text-lg px-4 py-2 rounded-sm shadow">Annuler
+                </button>
+            </div>
+        </div>
+
+        <div className="flex flex-col space-y-5 my-[5%]">
+
+            <input className="bg-white font-Lexend p-3 rounded-sm" placeholder="Entrez un titre"
+                   style={{color: "#626380"}}
+                   type="text"/>
+
+            <div className="flex space-x-5">
+
+                <textarea className="grow bg-white font-Lexend p-3 rounded-sm resize-none"
+                          placeholder="Ajouter une description"
+                          style={{color: "#626380"}}/>
+
+                <div className="grow flex flex-col space-y-5">
+
+                    <input className="bg-white font-Lexend p-3 rounded-sm" placeholder="Entrez des tags"
+                           style={{color: "#626380"}}
+                           type="text"/>
+                    <input className="bg-white font-Lexend p-3 rounded-sm" placeholder="Choisissez une date limite"
+                           style={{color: "#626380"}}
+                           type="date"/>
+
+                    <div className="flex justify-between">
+                        <div className="flex space-x-3">
+                            <input type="checkbox"/>
+                            <p>Deck éducatif</p>
+                        </div>
+                        <div className="flex space-x-3">
+                            <input type="checkbox"/>
+                            <p>Deck privé</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <hr className="my-[5%]"/>
+
+
+        <div className="flex flex-col space-y-8">
+
+            <button
+                className=" w-fit flex items-center gap-2 bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500 text-white font-Lexend text-lg px-4 py-2 rounded-sm shadow">
+                <img alt="" height={32} src="/magic.svg" width={32}/>
+                Générer
             </button>
-          </div>
-          <div className="flex flex-col gap-y-[0.2vh]">{elements}</div>
+
+            {cardsJSX}
+
+            <div className="flex space-x-3 justify-center">
+                <button onClick={addCard}
+                    className=" w-fit flex items-center gap-2 bg-gray-700 text-white font-Lexend text-lg px-4 py-2 rounded-sm shadow">
+                    <img alt="" height={20} src="/add_white.svg" width={20}/>
+                    Ajouter une nouvelle carte
+                </button>
+                <button className="bg-blue-500 text-white font-Lexend text-lg px-4 py-2 rounded-sm shadow">
+                    Créer
+                </button>
+            </div>
+
         </div>
-      </div>
-      <button
-        className="p-2 w-1/3 h-[6vh] bg-sky-700 text-white rounded-md col-span-2 text-2xl font-semibold"
-        onClick={HandleConfirm}
-        type="button"
-      >
-        Valider
-      </button>
+
     </div>
-  );
+    <Footer/>
+</div>
+
 }
 
-function CardPreview(c: Card, index: number, HandleDelete :()=>void): JSX.Element {
-  return (
-    <div key={c.id_card} className="flex items-center gap-2 bg-white border-gray justify-between px-2">
-      <Link
-        className="flex justify-between w-[90%] h-10 items-center"
-        href={{ pathname: "/newDeck/card", query: { id: c.id_card } }}
-        key={c.id_card}
-      >
-        <p>{index}</p>
-        <p className="w-1/2 overflow-ellipsis whitespace-nowrap overflow-hidden">
-          {c.question}
-        </p>
-        <p>palier : {c.palier}/5</p>
-      </Link>
-      <button className="p-1 bg-red-600 rounded-full size-6" onClick={HandleDelete} type="button">
-        <Image alt="" height={20} src="close.svg" width={20} />
-      </button>
-    </div>
-  );
-}
-
-function EmptyCards(): JSX.Element {
-  return <div className="text-gray-600">Ce deck ne contient pas encore de cartes ...</div>;
-}
+export default Page;
