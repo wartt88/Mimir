bad4fb46f7c66c46331df0fbef91c7d1588ef2c8
Alexandre P <alexandre.perrot54@gmail.com>
2024-01-23 09:54:57 +0100

    fix(marketplace): add carousel

--- app/apps/web/src/app/explore/page.tsx
+++ app/apps/web/src/app/explore/page.tsx
@@ -2,6 +2,14 @@
 import ResearchBar from "../../components/ui/research-bar.tsx";
 import {ChangeEvent} from "react";
 import {DeckUIPublic} from "../../components/ui/deck.tsx";
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious
+} from "../../components/ui/carousel.tsx";
+import UserPreviewGen from "../../components/ui/user-preview-gen.tsx";
 
 const Explore = (): JSX.Element => {
 
@@ -20,13 +28,25 @@ const Explore = (): JSX.Element => {
         <div className="flex flex-col items-center mt-10 space-y-10">
             <h1 className="font-Lexend text-3xl font-medium">Bibliothèque de decks</h1>
             <ResearchBar onChange={handleChange}/>
-            <div className="flex space-x-3">
-                <img src="/arrow_left.svg" alt="arrow"/>
-                <button className="bg-yellow-200 p-5 rounded-md font-Lexend text-sm">Tags</button>
-                <button className="bg-yellow-200 p-5 rounded-md font-Lexend text-sm">Tags</button>
-                <button className="bg-yellow-200 p-5 rounded-md font-Lexend text-sm">Tags</button>
-                <img src="/arrow_right.svg" alt="arrow"/>
+
+            <div className="flex flex-col size-full items-center">
+                <Carousel className="w-[75%]" id="carousel" opts={{align: "start"}}>
+                    <CarouselContent>
+                        <CarouselItem className="md:basis-1/2 lg:basis-1/3">
+                            <button className="bg-yellow-200 p-5 rounded-md font-Lexend text-sm">Tags</button>
+                        </CarouselItem>
+                        <CarouselItem className="md:basis-1/2 lg:basis-1/3">
+                            <button className="bg-yellow-200 p-5 rounded-md font-Lexend text-sm">Tags</button>
+                        </CarouselItem>
+                        <CarouselItem className="md:basis-1/2 lg:basis-1/3">
+                            <button className="bg-yellow-200 p-5 rounded-md font-Lexend text-sm">Tags</button>
+                        </CarouselItem>
+                    </CarouselContent>
+                    <CarouselPrevious/>
+                    <CarouselNext/>
+                </Carousel>
             </div>
+
             <div className="flex flex-wrap justify-center gap-3">
                 {decks}
             </div>
