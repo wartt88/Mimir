567d6e0b8097fc6093ecc0535ba704f9485f4797
Alexandre Perrot <alexandre.perrot54@gmail.com>
2024-01-24 17:53:10 +0100

    feat(contact): add search bar

--- app/apps/web/src/app/contact/page.tsx
+++ app/apps/web/src/app/contact/page.tsx
@@ -12,11 +12,16 @@ export default function Contact(): JSX.Element {
 
     const {data: session} = useSession();
 
-    const [contacts, setContacts] = useState<JSX.Element[]>([]);
-    const [users, setUsers] = useState<JSX.Element[]>([]);
+    const [initialContacts, setInitialContacts] = useState<UserInterface[]>([]);
+    const [initialUsers, setInitialUsers] = useState<UserInterface[]>([]);
+
     const [loaded, setLoaded] = useState(false);
 
     const [isGlobalSearching, setIsGlobalSearching] = useState(false);
+
+    const [contacts, setContacts] = useState<UserInterface[]>([]);
+    const [users, setUsers] = useState<UserInterface[]>([]);
+
 
     useEffect(() => {
         if (session?.user) {
@@ -24,41 +29,67 @@ export default function Contact(): JSX.Element {
                 if (!loaded) {
 
                     void (async () => {
-                        const contacts = await fetchContactCurrentUser(session.user.email);
-                        const jsxElements = contacts.map((e) => {
-                            return <UserPreviewGen user={e} key={e._id} type="friend"/>
-                        })
-                        setContacts(jsxElements)
-                    })();
+                        const dataContact = await fetchContactCurrentUser(session.user.email);
+                        setInitialContacts(dataContact);
+                        setContacts(dataContact);
 
-                    void (async () => {
-                        const contacts = await fetchAllUser();
-                        const jsxElements = contacts.map((e) => {
-                            return <UserPreviewGen user={e} key={e._id} type="user"/>
-                        })
-                        setUsers(jsxElements)
+                        const dataUser = await fetchAllUser();
+                        setInitialUsers(dataUser);
+                        setUsers(dataUser);
+
                     })();
 
                     setLoaded(true);
+
                 }
             }
         }
     }, [session]);
 
+
+    const userElements = users.map((e) => {
+        return <UserPreviewGen user={e} key={e._id} type="user"/>
+    })
+
+    const contactsElements = contacts.map((e) => {
+        return <UserPreviewGen user={e} key={e._id} type="friend"/>
+    })
+
+    const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value;
+        const filteredUsers = initialUsers.filter((e) => {
+            return e.username.toLowerCase().includes(value.toLowerCase())
+        })
+        setUsers(filteredUsers);
+
+        const filteredContacts = initialContacts.filter((e) => {
+            return e.username.toLowerCase().includes(value.toLowerCase())
+        })
+        setContacts(filteredContacts);
+    }
+
+
+    const title = isGlobalSearching ? "Ajouter un contact" : "Vos contacts";
+
     return (
         <div className="size-full">
             <div className="p-[5vh]">
                 <div className="flex flex-row justify-between">
-                    <h1 className="font-Lexend text-3xl font-medium">Vos contacts</h1>
-                    <button onClick={() => { setIsGlobalSearching(!isGlobalSearching) }}
+                    <h1 className="font-Lexend text-3xl font-medium">{title}</h1>
+                    <button onClick={() => {
+                        setIsGlobalSearching(!isGlobalSearching)
+                    }}
                             className="w-fit bg-black text-white font-Lexend text-lg px-4 py-2 rounded-sm shadow">
                         {isGlobalSearching ? <p>Voir ses contacts</p> : <p>Ajouter un contact</p>}
                     </button>
                 </div>
+                <div className="mt-10 flex justify-center">
+                    <ResearchBar onChange={handleSearch}/>
+                </div>
                 <div className="flex flex-col space-y-[8vh] mt-[5vh] items-center">
                     {loaded ?
                         <div className="w-full space-y-[5vh]">
-                            {isGlobalSearching ? users : <>{contacts.length > 0 ? contacts :
+                            {isGlobalSearching ? userElements : <>{contacts.length > 0 ? contactsElements :
                                 <p className="text-center font-Lexend text-xl">Vous n'avez pas de contacts</p>}</>}
                         </div>
                         :
