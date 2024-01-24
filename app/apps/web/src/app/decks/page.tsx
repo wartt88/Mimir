96329ededa7e6e53b220661d74ddbc62d60d314f
wartt88 <theo036188@gmail.com>
2024-01-24 09:10:58 +0100

    fix(Pages): bind BDD on deck's page

--- app/apps/web/src/app/decks/page.tsx
+++ app/apps/web/src/app/decks/page.tsx
@@ -1,10 +1,29 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 import DeckUI from "../../components/ui/deck-ui";
-import { DeckEmpty } from "../../models/deck";
+import { fetchDecks } from "../../models/deck-requests";
+import { type DeckInterface } from "../../models/deck";
+import Loader from "../../components/ui/loader";
 
 export default function Page(): JSX.Element {
+  const [elements, setElements] = useState<JSX.Element[]>([]);
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    if (!loaded) {
+      void (async () => {
+        const d: DeckInterface[] = await fetchDecks();
+        const jsxElements: JSX.Element[] = d
+          ? d.map((e) => <DeckUI deck={e} key={e._id} type="public" />)
+          : [<p key={100000}>Vous n'avez pas encore de decks</p>];
+        setElements(jsxElements);
+        setLoaded(true);
+      })();
+    }
+  }, []);
+
   return (
     <div className="w-[90%] size-full">
       <div className="flex justify-between w-[100%] my-20">
@@ -23,11 +42,8 @@ export default function Page(): JSX.Element {
           <p className="font-Lexend text-xl ml-2">Créer un nouveau deck</p>
         </Link>
       </div>
-      <div className="flex flex-wrap gap-3 justify-center">
-        <DeckUI deck={DeckEmpty} type="public" />
-        <DeckUI deck={DeckEmpty} type="perso" />
-        <DeckUI deck={DeckEmpty} type="stats" />
-      </div>
+      {loaded? <div className="flex flex-wrap gap-3 justify-center">{elements}</div> : <div className="h-[70%] flex items-center justify-center"><Loader/></div>}
+      
     </div>
   );
 }
