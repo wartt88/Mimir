522e6e6e9178fa137c98cc4bf9c75d99ecd16294
Alexandre P <alexandre.perrot54@gmail.com>
2024-01-30 09:26:29 +0100

    remove unused import

--- app/apps/web/src/app/accueil/page.tsx
+++ app/apps/web/src/app/accueil/page.tsx
@@ -1,17 +1,17 @@
 "use client";
 import Image from "next/image";
-import computer from "../../../public/pc.jpg";
 import Link from "next/link";
-import React, {ChangeEvent} from "react";
+import React from "react";
+import computer from "../../../public/pc.jpg";
 import NavBar from "../../components/ui/nav-bar.tsx";
 import ResearchBar from "../../components/ui/research-bar.tsx";
 import Footer from "../../components/ui/footer.tsx";
 import DeckUI from "../../components/ui/deck-ui.tsx";
-import {DeckInterface} from "../../models/deck.ts";
+import type {DeckInterface} from "../../models/deck.ts";
 
-const Preview = () => {
+function Preview() {
     return <div className="h-192 w-full relative">
-        <Image src={computer} fill alt="" className="object-cover"/>
+        <Image alt="" className="object-cover" fill src={computer}/>
         <div className="w-1/3 h-1/2 absolute top-1/4 left-12 ">
             <div className="flex flex-col space-y-10">
                 <h1 className="text-5xl font-bold font-Lexend text-white">Apprenez vos cours rapidement</h1>
@@ -20,8 +20,8 @@ const Preview = () => {
                     amet,
                     consectetur adipiscing elit. </p>
                 <div>
-                    <Link href="/register"
-                          className="font-Lexend text-lg bg-amber-500 text-white p-6 rounded-lg">Nous
+                    <Link className="font-Lexend text-lg bg-amber-500 text-white p-6 rounded-lg"
+                          href="/register">Nous
                         rejoindre</Link>
                 </div>
             </div>
@@ -29,7 +29,7 @@ const Preview = () => {
     </div>
 }
 
-const Home = () => {
+function Home() {
 
     const DeckEmpty: DeckInterface = {
         id: 0,
@@ -50,22 +50,22 @@ const Home = () => {
 
     const handleChange = () => {}
 
-    let element: any[] = []
-    for (let i: number = 0; i < 10; i++) {
-        element.push(<DeckUI type="public" deck={DeckEmpty} key={i}/>)
+    const element: any[] = []
+    for (let i = 0; i < 10; i++) {
+        element.push(<DeckUI deck={DeckEmpty} key={i} type="public"/>)
     }
 
     return (
         <div className="size-full">
             <NavBar/>
             <Preview/>
-            <div id="marketplace" className="flex flex-col items-center space-y-10">
+            <div className="flex flex-col items-center space-y-10" id="marketplace">
                 <h1 className="font-Lexend font-bold text-3xl mt-10">Explorer notre bibliothèque de decks</h1>
                 <ResearchBar onChange={handleChange}/>
                 <div className="flex flex-wrap gap-1 justify-center">
                     {element}
                 </div>
-                <a href="/register" className="font-Lexend px-5 py-3 bg-black text-white rounded-lg shadow">Voir
+                <a className="font-Lexend px-5 py-3 bg-black text-white rounded-lg shadow" href="/register">Voir
                     plus</a>
             </div>
             <Footer/>
