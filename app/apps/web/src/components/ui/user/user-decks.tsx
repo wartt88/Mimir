7358c71e7008841bfd297ed04fd44c00d05b85a0
Delioos <htz_j@protonmail.com>
2024-01-31 09:46:02 +0100

    ?(?):?

--- app/apps/web/src/components/ui/user/user-decks.tsx
+++ app/apps/web/src/components/ui/user/user-decks.tsx
@@ -1,12 +1,33 @@
 'use client'
 import { useSession } from "next-auth/react";
 import { DeckInterface } from "../../../models/deck";
-import { fetchCurrentUserDecks } from "../../../models/deck-requests";
+import { fetchCurrentUserDecks, fetchDeckByOwner } from "../../../models/deck-requests";
 import deckList from "../deck-list";
 import { useState, useEffect} from "react";
+import { UserInterface } from "../../../models/user";
 
 export default function UserDecks(): JSX.Element { 
+  const [userDecks, setUserDecks] = useState<DeckInterface[]>([]);
+  const [user, setUser] = useState<UserInterface|undefined>(undefined);
+  const [loaded, setLoaded] = useState(false);
+  const { data: session } = useSession();
 
+  useEffect(() => {
+    if (session?.user?.email && !user) {
+      void(async()=>{
+        const newUser:UserInterface = await fetchCurrentUser(session.user.email);
+        setUser(newUser);
+      })()
+    }
+  }, [session]);
+
+  useEffect(() => {
+    if (!loaded) {
+      void (async () => {
+        const d: DeckInterface[] = await fetchDeckByOwner(user._id);
+      })
+    }
+  }
   return (
     <div className="">
       <div className="flex justify-start my-10 px-10">
