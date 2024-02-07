ba061b294101778075f9b54787493c1d33003d78
Alexandre Perrot <alexandre.perrot54@gmail.com>
2024-02-07 18:57:08 +0100

    fix(import): remove unused imports

--- app/apps/web/src/app/(default)/layout.tsx
+++ app/apps/web/src/app/(default)/layout.tsx
@@ -1,6 +1,5 @@
 import "../globals.css";
 import React from "react";
-import {Menu} from "../../components/ui/menu.tsx";
 
 export default function SidebarLayout({children}: { children: React.ReactNode }): JSX.Element {
     return (
