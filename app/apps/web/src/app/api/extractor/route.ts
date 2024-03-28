3b48373c5b65c9d214388a3723864ae696be2435
Alexandre Perrot <alexandre.perrot54@gmail.com>
2024-03-28 20:29:27 +0100

    fix(ollama): add fail-safe protection for ollama

--- app/apps/web/src/app/api/extractor/route.ts
+++ app/apps/web/src/app/api/extractor/route.ts
@@ -3,17 +3,21 @@ import {NextResponse} from "next/server";
 export async function POST(req: Request) {
     const reqData = await req.formData();
 
-    const resData = await fetch(`http://extractor:5000/upload/`, {
-        method: "POST",
-        body: reqData,
-    }).then(response => response.json()
-    ).then(data => {
-        return data;
-    }).catch((err) => {
-        console.error(err);
-    });
+    if (process.env.OLLAMA_ENABLED) {
+        const resData = await fetch(`http://extractor:5000/upload/`, {
+            method: "POST",
+            body: reqData,
+        }).then(response => response.json()
+        ).then(data => {
+            return data;
+        }).catch((err) => {
+            console.error(err);
+        });
+        return NextResponse.json({ok: true, resData: resData}, {status: 201}); // learn whats a header
 
-    return NextResponse.json({resData}, {status: 201}); // learn whats a header
+    }
+
+    return NextResponse.json({ok: false}, {status: 201}); // learn whats a header
 
 }
 
